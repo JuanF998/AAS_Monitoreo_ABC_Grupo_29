44825666c36445d00e64db420b53b3cb4354c47f
Andres De la Hoz <andelahozg@gmail.com>
2022-09-10 19:46:46 -0500

    Microservicio Sensor Ambiente agregado

--- microservicio_localizacion/app.py
+++ microservicio_localizacion/app.py
@@ -1,7 +1,7 @@
 from microservicio_localizacion import create_app
 from flask_restful import Resource, Api
 from flask import Flask
-
+import requests
 
 app = create_app('default')
 app_context = app.app_context()
@@ -11,6 +11,14 @@ api = Api(app)
 
 class VistaEstadoLocalizaciones(Resource):
     def get(self):
-        return True
+        try:
+            content = requests.get('http://127.0.0.1:5003/sensor/estado')
+            
+            if content.status_code == 404:
+                return content.json(), 404
+            else:
+                return content.json()
+        except: 
+            return 'Conection lost'  
 
 api.add_resource(VistaEstadoLocalizaciones, '/localizaciones/estado')
